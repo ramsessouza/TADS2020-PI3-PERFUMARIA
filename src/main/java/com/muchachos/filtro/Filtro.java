ea72ab70af4d020d07a2187d48a40101fcd2cf93
ramses.souza@hotmail.com <>
2020-06-11 00:17:46 -0300

    Filtro maroto, simples e eficiente, falta terminar as regras de usuário, mas ja evita acessar as paginas pela url;

---
+++ src/main/java/com/muchachos/filtro/Filtro.java
@@ -0,0 +1,63 @@
+package com.muchachos.filtro;
+
+import com.muchachos.model.Funcionario;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 10/06/2020 23:26
+ * @author Ramses
+ */
+@WebFilter(filterName = "Filtro", urlPatterns = {"/*"})
+public class Filtro implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String urlAcessada = httpRequest.getRequestURI();
+        
+        //se a tela acessada for diferente de index.jsp
+        if(!urlAcessada.contains("/index")){
+            //Verifica se usuario ja esta deslogado
+            HttpSession sessao = httpRequest.getSession();
+            if(sessao.getAttribute("funcionarioSessao") == null){
+                //se estiver deslogado envia para tela de login
+                httpResponse.sendRedirect(httpRequest.getContextPath() + "/index.jsp");
+                return;
+            }
+
+            //verifica se tem acesso a pagina especifica (TEM QUE TERMINAR ESSA PARTE)
+            Funcionario funcionarioLogado = (Funcionario) sessao.getAttribute("funcionarioSessao");
+            verificarAcesso(funcionarioLogado, httpRequest, urlAcessada);
+        }
+        //segue com a solicitacao
+        chain.doFilter(request, response);
+    }
+    
+    private boolean verificarAcesso(Funcionario funcionario, HttpServletRequest httpRequest, String urlAcessada){
+        return false;
+    }
+    
+    @Override
+    public void destroy() {        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {        
+    }
+}
